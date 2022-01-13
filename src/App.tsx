68b2b0995c4721e99d12a41522e65b200770461b
Svitlana <devop.edu@gmail.com>
2022-01-13 12:36:29 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './assets/sass/App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     Start
+    </div>
+  )
+}
+
+export default App
