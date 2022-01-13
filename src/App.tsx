8357dfca1f7af19074f145383a1ff3cb010031fe
Svitlana <devop.edu@gmail.com>
2022-01-13 16:19:46 +0100

    created Todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React, { useState } from 'react'
-import './assets/sass/App.scss'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-     Start
-    </div>
-  )
+interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+  image?: string | null | undefined;
 }
 
-export default App
+const initialTodos: Todo[] = [
+  { id: "123", title: "Learn JS", completed: true },
+  { id: "345", title: "Learn React", completed: true },
+  { id: "567", title: "Learn TypeScript", completed: false },
+];
+
+function App() {
+  // ***init array of numbers
+  // const higscores:number[]=[]
+  // ***init array without types
+  // const anyArr: any[]=[]
+  // anyArr[0] = 12
+  // anyArr[1]="12"
+
+  const [count, setCount] = useState(initialTodos);
+
+  return (
+    <Container id="App" className="my-3">
+     <h1>Todos</h1>
+      {count.map((el) => (
+        <ul key={el.id}>
+          <li>{el.title}</li>
+        </ul>
+      ))}
+    </Container>
+  );
+}
+
+export default App;
